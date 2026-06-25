52a508d9a39731374f99c4b2d55172b053446766
petro <petromarik@gmail.com>
2026-06-25 14:21:59 -0400

    Added initial

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-package mainimport (	"fmt"	"log"	"net/http")func main() {	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {		response(w, r)	})	log.Println("server started on :8080")	log.Fatal(http.ListenAndServe(":8080", nil))}func response(w http.ResponseWriter, r *http.Request) {	_, err := fmt.Fprintln(w, "Hello from Go on Elastic Beanstalk")	if err != nil {		log.Fatal(err)	}}
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		response(w, r)
+	})
+
+	log.Println("server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func response(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintln(w, "Hello from Go on Elastic Beanstalk")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- main_test.go
+++ main_test.go
@@ -1,1 +1,25 @@
-package mainimport (	"net/http"	"testing")type MockedResponseWriter struct {	http.ResponseWriter	buf string}func (w *MockedResponseWriter) Write(b []byte) (int, error) {	w.buf = string(b)	return len(b), nil}func TestMainFunc(t *testing.T) {	rw := &MockedResponseWriter{}	response(rw, nil)	if rw.buf != "Hello from Go on Elastic Beanstalk\n" {		t.Fatal(rw.buf, "!=", "Hello from Go on Elastic Beanstalk")	}}
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+type MockedResponseWriter struct {
+	http.ResponseWriter
+	buf string
+}
+
+func (w *MockedResponseWriter) Write(b []byte) (int, error) {
+	w.buf = string(b)
+	return len(b), nil
+}
+
+func TestMainFunc(t *testing.T) {
+	rw := &MockedResponseWriter{}
+	response(rw, nil)
+
+	if rw.buf != "Hello from Go on Elastic Beanstalk\n" {
+		t.Fatal(rw.buf, "!=", "Hello from Go on Elastic Beanstalk")
+	}
+}
